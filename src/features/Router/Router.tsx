d1b14e411869113a1b3ec6fa4373fc99777c8958
Андрей Стригалев <andrewstampede@MacBook-Pro-Andrej.local>
2020-12-20 19:27:56 +0300

    Fix

--- src/features/Router/Router.tsx
+++ src/features/Router/Router.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
 import FirstWeek from '../../pages/FirstWeek';
 import SecondWeek from '../../pages/SecondWeek';
-import WelcomePage from '../../pages/WelcomePage'
-
+import WelcomePage from '../../pages/WelcomePage';
 
 const Router: React.FC = () => {
   return (
     <>
-    <WelcomePage />
-     <FirstWeek />
-     <SecondWeek />
+      <WelcomePage />
+      <FirstWeek />
+      <SecondWeek />
+      <Route path='*'>
+        <Redirect to='/' />
+      </Route>
     </>
   );
 };
